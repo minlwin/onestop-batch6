82c0bfad5f70632136ff4419c1363bc06e97d256
Min Lwin <lwin.zawmin@gmail.com>
2023-12-21 04:24:52 +0630

    Update API Result for Issue Date Time

--- s02-backend/member-api/src/main/java/com/jdc/shop/utils/io/ApiResponse.java
+++ s02-backend/member-api/src/main/java/com/jdc/shop/utils/io/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.jdc.shop.utils.io;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,27 +12,27 @@ import lombok.NoArgsConstructor;
 public class ApiResponse<T> {
 
 	private Status status;
-
+	private LocalDateTime issueAt;
 	private T payload;
 	
 	public static <T> ApiResponse<T> success(T data) {
-		return new ApiResponse<T>(Status.Success, data);
+		return new ApiResponse<T>(Status.Success, LocalDateTime.now(), data);
 	}
 
 	public static <T> ApiResponse<T> validationError(T data) {
-		return new ApiResponse<T>(Status.ValidationError, data);
+		return new ApiResponse<T>(Status.ValidationError, LocalDateTime.now(), data);
 	}
 
 	public static <T> ApiResponse<T> businessError(T data) {
-		return new ApiResponse<T>(Status.BusinessError, data);
+		return new ApiResponse<T>(Status.BusinessError, LocalDateTime.now(), data);
 	}
 
 	public static <T> ApiResponse<T> platformError(T data) {
-		return new ApiResponse<T>(Status.PlatformError, data);
+		return new ApiResponse<T>(Status.PlatformError, LocalDateTime.now(), data);
 	}
 
 	public static <T> ApiResponse<T> securityError(T data) {
-		return new ApiResponse<T>(Status.SecurityError, data);
+		return new ApiResponse<T>(Status.SecurityError, LocalDateTime.now(), data);
 	}
 
 	public enum Status {
